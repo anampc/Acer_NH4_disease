5b2b9d5327969ed4c4c408b6498c6e40cbd910c1
Ana Palacio <ana.palacioc@gmail.com>
2024-02-13 08:43:46 -0500

    added qPCR data

---
+++ 6.Symbiodiniaceae/Acerv_Nutrients_project1/SH_analysis.R
@@ -0,0 +1,265 @@
+
+# set working directory. This wd is different than the one used for calculating S/H
+setwd("C:/Users/12677/OneDrive - University of Miami/Desktop/Acerv_Nutrients_project1")
+
+# load packages
+library(readr)
+library(ggplot2)
+library(Rmisc)
+
+# Hypothesis #1: Corals in NH4 treatment will have elevated S/H
+
+#clear environment
+rm(list=ls())
+
+# read in data file(s)
+NDSH = read.csv("qPCR samples_R.csv")
+
+#subset samples into before and after disease exposure
+b4disease = subset(NDSH, Sequence=="OnlyNutrients")
+
+afterdisease = subset(NDSH, Sequence=="Disease")
+
+# scatterplot of S/H after nutrients but before disease
+theme_set (theme_classic() + theme(panel.grid.major = element_blank(),
+                                   panel.grid.minor = element_blank(), 
+                                   axis.line = element_line(colour = "black"),
+                                   legend.position="bottom",
+                                   axis.text.x = element_text(angle = 90, vjust = 0.5),
+                                   plot.title = element_text(size=12, face="bold"),
+                                   #panel.border = element_rect(colour = "black", fill=NA, size=1)
+                                   panel.border = element_blank()
+))
+
+SHb4scatter <- ggplot(aes(x=Tag, y=A.Acerv, colour=Nutrients), data = b4disease,) +
+  geom_point() +
+  labs(title = "Symbiont Abundance by Treatment",
+       y = "S/H Ratio", x = "Fragment #")
+SHb4scatter
+
+#before disease scatter plot by genotype, grouped by treatment
+SHb4genoscatter <- ggplot(aes(x=Tag, y=A.Acerv, colour=Genotype, group=Nutrients), data = b4disease,) +
+  geom_point() +
+  facet_wrap(~ Nutrients)+
+labs(title = "Symbiont Abundance by Genotype",
+     y = "S/H Ratio", x = "Fragment #")
+SHb4genoscatter
+
+
+# after disease exposure scatter plot of S/H
+SHafterscatter <- ggplot(aes(x=Tag, y=A.Acerv, colour=Nutrients), data = afterdisease) +
+  geom_point() +
+  labs(title = "Symbiont Abundance After Disease Exposure",
+       y = "S/H Ratio", x = "Fragment #")
+SHafterscatter
+
+# Before and after disease scatter plots (not sure I'll actually include this)
+SHb4afterscatter <- ggplot(aes(x=Tag, y=A.Acerv, colour=Nutrients), data = NDSH) +
+  geom_point() +
+  facet_wrap(~Timepoint)+
+  labs(title = "Symbiont Abundance After Nutrients, Disease Exposure",
+       y = "S/H Ratio", x = "Fragment #")
+SHb4afterscatter
+
+# Basic box plot for before and after disease (not sure I'll include this either)
+SHboxb4 <- ggplot(b4disease, aes(x=Nutrients, y=A.Acerv)) + 
+  geom_boxplot()+
+  labs(title="Symbiont Abundance after Six Weeks of Elevated Ammonium", y="S/H Ratio", x="Treatment")
+SHboxb4
+
+SHboxafter <- ggplot(afterdisease, aes(x=Nutrients, y=A.Acerv)) + 
+  geom_boxplot()
+SHboxafter
+
+SHboxplots <- ggplot(NDSH, aes(x=Nutrients, y=A.Acerv)) + 
+  geom_boxplot()+
+  facet_wrap(~ Timepoint)
+SHboxplots
+labs(title = "",
+     y = "S/H Ratio", x = "Nutrients Treatment")
+
+#Using tapply to find the average S/H for each Nutrients treatment
+tapply(b4disease$A.Acerv,b4disease$Nutrients,mean)
+tapply(afterdisease$A.Acerv, afterdisease$Nutrients,mean)
+
+# summary of data before disease exposure
+summaryb4=summarySE(b4disease,measurevar = 'A.Acerv',groupvars = c('Nutrients'))
+summaryb4
+
+#summary of data after disease exposure
+summaryafter=summarySE(afterdisease,measurevar='A.Acerv',groupvars=c('Nutrients'))
+summaryafter
+
+# bar graph of Ambient vs NH4 treatments before disease exposure
+SHbarb4 <- ggplot(summaryb4, aes(x=Nutrients, y=A.Acerv))
+SHbarb4 <- SHbarb4+geom_col(fill='lightgray')
+SHbarb4 =SHbarb4+geom_errorbar(aes(ymin=A.Acerv-se, ymax=A.Acerv+se),width=.1,size=0.5)+
+labs(title = "Symbiont Abundance by Treatment",
+       y = "S/H Ratio", x = "Nutrients Treatment")
+SHbarb4
+
+# bar graph of Ambient vs NH4 treatments after diease exposure
+SHbarafter <- ggplot(summaryafter, aes(x=Nutrients, y=A.Acerv))
+SHbarafter <- SHbarafter+geom_col(fill='lightgray')
+SHbarafter =SHbarafter+geom_errorbar(aes(ymin=A.Acerv-se, ymax=A.Acerv+se),width=.1,size=0.5)
+SHbarafter
+
+# using tapply to determine if variance is the same before disease
+tapply(b4disease$A.Acerv, b4disease$Nutrients, var)
+
+# variances are approximately the same. We can use the Bartlett test to test if variances are significantly different 
+bartlett.test(b4disease$A.Acerv ~ b4disease$Nutrients)
+# Bartlett test p-value = 0.1876, variances are not significantly different. So we can use a parametric test
+
+# ANOVA
+b4ANOVA=aov(A.Acerv~Nutrients,b4disease)
+summary(b4ANOVA)
+
+# Shapiro Test for normal distribution
+shapiro.test(resid(b4ANOVA))
+
+# qq-plot
+qqnorm(resid(b4ANOVA))
+qqline(resid(b4ANOVA,lty=2))
+
+### Do a non-parametric instead: 2 sided Wilcoxon Sum Rank Test
+### Distribution is not normal. 
+?wilcox.test()
+wilcox.test(b4disease$A.Acerv ~ b4disease$Nutrients, var.equal=TRUE)
+
+# p-value = 0.7469 is not below 0.05, no significant difference
+
+# specific Wilcox Test to see if S/H for Ambient is significantly greater than NH4
+# create vectors for each treatment
+b4Ambient=b4disease$A.Acerv[b4disease$Nutrients=='Ambient']
+b4Ambient
+
+b4NH4=b4disease$A.Acerv[b4disease$Nutrients=='NH4']
+b4NH4
+
+# specific t-test to see if S/H for Ambient is significantly greater than NH4 after diease exposure
+# create vectors for each treatment
+afterAmbient=afterdisease$A.Acerv[afterdisease$Nutrients=='Ambient']
+afterAmbient
+
+afterNH4=afterdisease$A.Acerv[afterdisease$Nutrients=='NH4']
+afterNH4
+
+#now we can do a one-sided t-test for before disease exposure
+wilcox.test(b4Ambient,b4NH4,alternative = 'greater',var.equal=TRUE)
+#report the results as "A one-sided T-test found that the S/H ratio is not significantly higher for corals exposed to the Ambient treatment than the NH4 treatment.
+
+#same thing for after disease exposure. One sided t-test
+t.test(afterAmbient,afterNH4,alternative = 'greater',var.equal=TRUE)
+
+# summary of data before disease exposure by genotype
+summaryb4geno=summarySE(b4disease,measurevar = 'A.Acerv',groupvars = c('Genotype','Nutrients'))
+summaryb4geno
+
+#grouped bar plot by genotype
+genobar = ggplot(summaryb4geno, aes(fill=Nutrients, y=A.Acerv, x=Genotype)) + 
+  geom_bar(position='dodge', stat='identity')
+
+genobar = genobar+geom_errorbar(aes(ymin=A.Acerv-se, ymax=A.Acerv+se), width=.1, size= 0.5, 
+              position=position_dodge(.9))+
+  labs(title = "Symbiont Abundance After Nutrient Treatments",
+       y = "S/H Ratio", x = "Genotype")
+genobar
+
+genoANOVA=aov(A.Acerv~Genotype:Nutrients,b4disease)
+summary(genoANOVA)
+
+genoANOVA2=aov(A.Acerv~Genotype+Nutrients,b4disease)
+summary(genoANOVA2)
+
+TukeyHSD(genoANOVA, conf.level = 0.95)
+TukeyHSD(genoANOVA2, conf.level = 0.95)
+
+#do a genral mixed model
+?glmm
+#fuck that, I googled it and cannot figure it out
+
+
+#grouped bar by genotype and tank (not enough replication per tank for error bars)
+summarygenotank=summarySE(b4disease,measurevar = 'A.Acerv',groupvars = c('Genotype','Tank_D1'))
+summarygenotank
+
+class(summarygenotank$Tank_D1)="character"
+
+genotank = ggplot(summarygenotank, aes(fill=Tank_D1, y=A.Acerv, x=Genotype)) + 
+  geom_bar(position='dodge', stat='identity')
+
+genotank = genotank+geom_errorbar(aes(ymin=A.Acerv-se, ymax=A.Acerv+se), width=.1, size= 0.5, 
+                                position=position_dodge(.9))+
+  labs(title = "Symbiont Abundance After Nutrient Treatments",
+       y = "S/H Ratio", x = "Genotype")
+genotank
+
+# average S/H by tank
+summarytank=summarySE(b4disease,measurevar = 'A.Acerv',groupvars = c('Tank_D1'))
+summarytank
+
+class(summarytank$Tank_D1)="character"
+
+SHtank <- ggplot(summarytank, aes(x=Tank_D1, y=A.Acerv))
+SHtank <- SHtank+geom_col(fill='lightgray')
+SHtank =SHtank+geom_errorbar(aes(ymin=A.Acerv-se, ymax=A.Acerv+se),width=.1,size=0.5)+
+  labs(title = "Mean Symbiont Abundance by Tank",
+       y = "S/H Ratio", x = "Tank")
+
+SHtank
+
+
+#######################################################
+
+
+
+# Hypothesis #2: Higher algal densities = higher disease mortality
+
+# S/H vs Mortality scatter plot
+mortalityscatter <- ggplot(aes(x=Tag, y=A.Acerv, colour=Mortality), data = b4disease,) +
+  geom_point() 
+mortalityscatter
+
+#### Rich's suggestion: parse them out by survivorship, using nutrients as a predictor, and use S/H and genotype as co-variates 
+
+# step 1: parse out by survivorship
+
+
+#subset samples taken before disease exposure by Mortality fates
+?subset
+b4lived = subset(b4disease, Mortality=='Lived')
+b4lived
+
+b4died = subset(b4disease, Mortality=='Died')
+b4died
+
+# summary of S/H for corals the survived and died. S/H is almost the same for all. 
+summarylived=summarySE(b4lived,measurevar = 'A.Acerv',groupvars = c('Nutrients'))
+summarylived
+
+summarydied=summarySE(b4died,measurevar='A.Acerv', groupvars=c('Nutrients'))
+summarydied
+
+mort_count = tapply(b4disease$Mortality,b4disease$Nutrients,count)
+
+# graph of mortality by genotype
+mortalitybar=ggplot(b4disease,aes(x=Mortality))
+mortalitybar=mortalitybar+geom_bar(aes(y = (..count..)/sum(..count..)))  
+mortalitybar=mortalitybar+scale_y_continuous(labels=scales::percent)
+mortalitybar=mortalitybar+labs(y='Mortality, %')+
+facet_wrap(~ Nutrients)
+mortalitybar
+
+# mortality over time
+### error
+class(b4disease$Date.of.Mortality)="date"
+
+
+ggplot(data = b4disease, aes(Date.of.Mortality, n, color = Genotype)) +
+  geom_line(size = 1) +
+  geom_point() + 
+  labs(title = "Mortality by Genotype",
+       y = "Count of coral deaths", x = "")
+
+    
---
+++ 6.Symbiodiniaceae/Acerv_Nutrients_project1/Survival Analysis.R
@@ -0,0 +1,347 @@
+# set working directory. This wd is different than the one used for calculating S/H
+setwd("C:/Users/12677/OneDrive - University of Miami/Desktop/Acerv_Nutrients_project1")
+
+# load packages
+library(readr)
+library(ggplot2)
+library(Rmisc)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(lubridate)
+library(stringr)
+library(survminer)
+library(survival)
+
+# Survival analysis lets you analyze the rates of occurrence of events over time, without assuming the rates are constant.
+#lets you model the time until an event occurs, or compare the time-to-event between different groups, or how time-to-event correlates with quantitative variables.
+
+#clear environment
+rm(list=ls())
+
+# read in data file(s)
+NDSH = read_csv("qPCR samples_R.csv") #S/H data
+ND_all = read_csv("All_ND_R.csv") # all frags survivorship
+
+#format dates, added binary variable "category" to NDSH, 
+# added "SurvivalTime" variable, added "SH" variable (A.Acerv on log10 scale)
+NDSH <-  NDSH %>% 
+  mutate(
+    `Date Disease` = mdy(`Date Disease`), 
+    `Date of Mortality` = mdy(`Date of Mortality`),
+    survivalTime= `Date of Mortality`- `Date Disease`,
+    survivalTime=ifelse(is.na(survivalTime), 11, survivalTime), 
+    category=ifelse(Mortality=="Lived", 0, 1),
+    SH=log10(A.Acerv)
+  )
+
+# nutrients, diseased, and combo variables in NDSH converted from "character" type data to "factor" type
+NDSH$Combo <- as.factor(NDSH$Combo)
+NDSH$Diseased <- as.factor(NDSH$Diseased)
+NDSH$Nutrients <- as.factor(NDSH$Nutrients)
+
+
+# same thing as above for ND_all data frame
+ND_all <-  ND_all %>% 
+  mutate(
+    survivalTime= `Days Survived`,
+    category=ifelse(Mortality=="Lived", 0, 1),
+    Disease=as.factor(Disease),
+    Nutrients=as.factor(Nutrients)
+  )
+
+ND_all$Disease <- as.factor(ND_all$Disease)
+ND_all$Nutrients <- as.factor(ND_all$Nutrients)
+
+#################################################################################
+
+# ***********NDSH data frame**************
+
+#  "coxph" = Cox Fit Proportional Hazards Regression Model
+# this one is a Cox PH Model of disease and S/H Ratio on survival time
+# NOTE: Tank 2 already removed
+?coxph
+
+#tell R treatment order. order data frame 
+levels(NDSH$Diseased)
+?factor
+NDSH$Diseased <- factor(NDSH$Diseased, levels=c('Placebo', 'Pathogen'))
+levels(NDSH$Diseased)
+
+# do stats for each variable separately - Ana suggestion
+
+# Coxph #1: disease+SH
+# Disease matters for survival, SH doesn't 
+survMod_dis_SH <- coxph(Surv(survivalTime, category)~Diseased+SH, data=NDSH)
+survMod_dis_SH
+summary(survMod_dis_SH)
+
+# Coxph #2: diseased*SH
+survMod_dis_SH2 <- coxph(Surv(survivalTime, category)~Diseased*SH, data=NDSH)
+survMod_dis_SH2
+summary(survMod_dis_SH2)
+
+# Kaplan-Meier Plot #1: survival probability curve of pathogen vs. placebo over time
+# *** better if done with ND_all data
+ggsurvplot(fit = survfit(Surv(survivalTime, category)~Diseased, data=NDSH))+
+labs(title="A. cervicornis Survivorship by Disease Treatment")
+
+# datSH is a subset of NDSH with only Pathogen treatment fragments. 
+datSH <- NDSH %>%
+  filter(Diseased=='Pathogen', Timepoint=="T1")
+
+# Coxph #3: of just disease treatment fragments. SH not important for survival within the disease treatment.
+shMod <- coxph(Surv(survivalTime, category)~SH, data=datSH)
+summary(shMod)
+
+
+# This shows how SH does not predict relative risk
+predSH <- expand.grid(SH=-seq(.5, 1.3, 0.05))
+fitSH <- data.frame(predict(shMod, type='risk', se.fit=TRUE, newdata=predSH))
+fitSH <- cbind(predSH, fitSH) 
+
+ggplot(fitSH, aes(SH, fit)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=fit-se.fit, ymax=fit+se.fit), alpha=0.3) +
+  geom_hline(yintercept = 1, color='red')
+
+#######################################################################################
+
+# ***********ND_all data frame**************
+
+# This has more data than NDSH. 
+# Use this for everything except SH (for obvious reasons)
+
+# order ND_all$Disease
+levels(ND_all$Disease)
+?factor
+ND_all$Disease <- factor(ND_all$Disease, levels=c('Placebo', 'Pathogen'))
+levels(NDSH$Diseased)
+
+# Coxph #4: don't use this because tank 2 was not removed yet. Redo with tank 2 removed
+survMod <- coxph(Surv(survivalTime, category)~Disease*Nutrients, data=ND_all)
+summary(survMod)
+survdiff(Surv(survivalTime, category)~Disease+Nutrients, data=ND_all)
+ggsurvplot(fit = survfit(Surv(survivalTime, category)~Disease+Nutrients, data=ND_all))+
+labs(title = "A. cervicornis Survivorship by Nutrients-Disease Combination")
+
+###############################################################################
+
+#Tank 2 taken out for everything below. This is important because tank 2 behaved strangely (probiotic? idk)
+
+#Take out tank 2
+dat <- ND_all %>%
+  filter(Tank!=2| is.na(Tank))
+
+#mutate(Treatment=ifelse(Tank==2, "Probiotic", "Nothing"))
+
+#tell R treatment order. order data frame 
+levels(dat$Disease)
+dat$Disease <- factor(dat$Disease, levels=c('Placebo', 'Pathogen'))
+levels(NDSH$Diseased)
+
+# Coxph #5: of Nutrients Disease interaction ******** use this
+survMod <- coxph(Surv(survivalTime, category)~Disease*Nutrients, data=dat)
+survMod
+summary(survMod)
+
+# Coxph #6: Just disease vs placebo ***** use this
+survMod2 <- coxph(Surv(survivalTime, category)~Disease, data=dat)
+survMod2
+summary(survMod2)
+
+#***Figure 10***
+# Kaplan-Meier Plot #2: survival probability curve of pathogen vs. placebo over time
+ggsurvplot(fit = survfit(Surv(survivalTime, category)~Disease, data=dat))+
+  labs(title="A. cervicornis Survivorship by Disease Treatment")
+
+#***Figure 11***
+# Kaplan-Meier plot #3: Nutrients and disease treatment w. tank 2 removed
+Fill.colour<-c ("light blue", "dark blue","red","dark red")
+
+All<-ggsurvplot((fit = survfit(Surv(survivalTime, category)~Disease+Nutrients, data=dat)), 
+                palette=Fill.colour)+
+  labs(title = "A. cervicornis Survivorship by Nutrients-Disease Combination")
+All  
+
+# Kaplan-Meier plot #3: "Probiotic" plot ******* put this on hold for now
+datPro <- ND_all %>%
+  mutate(Treatment=as.factor(ifelse(Tank==2, "Probiotic", "noPri")))
+
+proMod <- coxph(Surv(survivalTime, category)~Disease+Treatment, data=datPro)
+summary(proMod)
+
+ggsurvplot(fit = survfit(Surv(survivalTime, category)~Disease+Treatment, data=datPro))
+
+
+#******* If I have time, maybe make some nutrients, disease survival plots using Ana's data****
+# lots of mortality during Nutrients only phase. Including that might make this make more sense
+
+######################################################################################################
+
+# make a plot with seperate survivorship curves for each genotype
+
+#below copied and edited from Ana's github
+
+# Genotype-Treatment model
+
+dat$Treatment<-paste(dat$Nutrients, dat$Disease, sep = "-" )
+dat <- dat %>%
+  filter(Genotype!="U41"| is.na(Genotype))
+
+# Kaplan-Meier estimator. The "log-log" confidence interval is preferred.
+fit2 <- survfit(Surv(survivalTime, category) ~ Genotype + Treatment, data = dat)
+summary(fit2)
+
+# Disease phase by Genotype
+#***Figure 12***
+#(Put in lines manually in Powerpoint)
+GenTre<-ggsurvplot(fit2, data = dat, pval = TRUE,
+                   risk.table=F,  tables.height=0.5)
+GenTre
+
+ggsurvplot_facet(fit2, data = dat, facet.by="Treatment", 
+                 #risk.table=T, tables.height=0.5, 
+                 nrow = 6, alpha=1,
+                 linetype=1) +
+  geom_vline(xintercept = 46, linetype="dashed", color = "gray")
+
+ggsurvplot_facet(fit2, data = dat, 
+                 facet.by="Genotype", 
+                 # risk.table=T, tables.height=0.5, 
+                 nrow = 3, alpha=1, linetype=1) +
+  geom_vline(xintercept = 46, linetype="dashed", color = "gray")+
+  scale_color_manual(values = c("red", "light blue","dark red","dark blue"))+
+  labs(title="Survivorship by Genotype: Disease Phase")
+
+# Cox PH #6: Genotype*Treatment ********** genuinely confused about what I did here. 
+survMod_geno <- coxph(Surv(survivalTime, category)~Genotype*Treatment, data=dat)
+survMod_geno
+summary(survMod_geno)
+
+# Cox PH #7:
+survMod_geno2 <- coxph(Surv(survivalTime, category)~Genotype+Treatment, data=dat)
+survMod_geno2
+summary(survMod_geno2)
+
+# Cox PH #7:
+survMod_geno3 <- coxph(Surv(survivalTime, category)~Genotype, data=dat)
+survMod_geno3
+summary(survMod_geno3)
+
+
+#######################################################################################
+
+# same thing as above, using Ana's .csv so that it includes nutrients only phase 
+
+# Import and organize data
+
+# Data
+Survival.data<-read.csv("Acer_Mortality2.csv", header = TRUE)
+summary(Survival.data)
+Survival.data$Date<-as.Date(Survival.data$Date)
+Survival.data$Day<-as.numeric(Survival.data$Date)-18518
+Survival.data$Fu.time_texp<-Survival.data$Day
+Survival.data$Treatment<-paste(Survival.data$Nutrients, Survival.data$Disease, sep = "-" )
+Survival.data <- Survival.data %>%
+  filter(Genotype!="U41"| is.na(Genotype))
+summary(Survival.data$Genotype)
+
+#Survival.data$Genotype<-factor 
+#   (Survival.data$Genotype, 
+#   levels=c("G_48", "G_62","G_31", "G_08","G_07", "G_50"))
+Survival.data$Genotype<-as.factor(Survival.data$Genotype)
+summary(Survival.data$Genotype)
+
+## Model 2 (Genotype-Treatemnt) with experimnatl data (excluding cooked tank)
+
+# Filter data 
+Survival.data_1<-Survival.data
+Survival.data_1<-filter(Survival.data_1, Treatment!="Ambient-Heat")
+Survival.data_1$Treatment<-factor(Survival.data_1$Treatment,
+                                  levels = c("Ambient-Placebo", "Ambient-Pathogen",
+                                             "NH4-Placebo", "NH4-Pathogen"))
+Survival.data_1$Genotype<-factor(Survival.data_1$Genotype, 
+                                 levels=c("FM19", "U44","FM6", "FM9","FM14", "Elkhorn",
+                                          "K2", "Acerv2", "Kelsey-1", "Cooper-9"))
+summary(Survival.data_1$Genotype)
+summary(Survival.data_1$Treatment)
+
+# Create survival object (Fit survival data using the Kaplan-Meier method)
+surv_object_1 <- Surv(time = Survival.data_1$Fu.time_texp, 
+                      event = Survival.data_1$Fu.stat_exp)
+surv_object_1 
+
+# Kaplan-Meier estimator. The "log-log" confidence interval is- preferred.
+
+fit1 <- survfit(surv_object_1 ~ Treatment, data = Survival.data_1)
+summary(fit1)
+
+fit2 <- survfit(surv_object_1 ~ Genotype + Treatment, data = Survival.data_1)
+summary(fit2)
+
+# Genotype plot made by Ana. Not used in report. Made similar plot with panels for each genotype instead so there are less survivorship curves per plot to follow.
+GenTre_1<-ggsurvplot(fit2, data = Survival.data_1, pval = TRUE,
+                     risk.table=F,  tables.height=0.5)
+GenTre_1
+
+GenotypeP<-ggsurvplot_facet(fit2, data = Survival.data_1, facet.by="Treatment", 
+                            risk.table=F, # tables.height=0.5, 
+                            nrow = 6, alpha=1, linetype=1) +
+  geom_vline(xintercept = 46, linetype="dashed", 
+             color = "gray")
+GenotypeP
+
+
+# Survival plot with panes by genotype
+#***Figure 14***
+TreatmentP<-ggsurvplot_facet(fit2, data = Survival.data_1, 
+                             facet.by="Genotype", 
+                             # risk.table=T, tables.height=0.5, 
+                             nrow = 3, alpha=1,
+                             linetype=1) +
+  geom_vline(xintercept = 48, linetype="dashed", color = "gray") +
+  scale_color_manual(values = c("light blue", "red","dark blue","dark red"))+
+  labs(title="Survivorship by Genotype: Nutrients and Disease Phases")
+TreatmentP
+
+# Kaplan-Meier plot using Ana's survivorship data to include nutrients and disease experiment phases
+#***Figure 13***
+Fill.colour<-c ("light blue", "red","dark blue","dark red")
+
+All_genotypes<-ggsurvplot(fit1, data=Survival.data_1,
+                          pval = TRUE, conf.int = F, risk.table=F,
+                          palette=Fill.colour,
+                          break.time.by=7, xlim=c(0,60)) +
+  ggtitle("A. cervicornis Survivorship by Nutrients-Disease Combination")
+All_genotypes
+
+# Coxph: of Nutrients Disease interaction
+survMod_ana <- coxph(Surv(Day, Fu.stat_exp)~Disease+Nutrients, data=Survival.data_1)
+survMod_ana
+summary(survMod_ana)
+
+
+#########################################################################################
+#Trying to change colors and add lines
+#copying it here and leaving originals alone in case I fuck up
+
+# Treatment model
+
+dat$Treatment<-paste(dat$Nutrients, dat$Disease, sep = "-" )
+
+# Kaplan-Meier plot by treatment using Ana's data
+ggsurvplot(fit = survfit(Surv(Day, Fu.stat_exp)~Treatment, data=Survival.data_1))+
+  scale_color_manual(values = c("light blue", "red","dark blue","dark red"))+
+  geom_vline(xintercept = 46, linetype="dashed", 
+             color = "gray")+
+  labs(title = "A. cervicornis Survivorship by Nutrients-Disease Combination")
+
+## It won't let me change the color or add lines.
+## Error message: Error in ggsurvplot(fit = survfit(Surv(Day, Fu.stat_exp) ~ Treatment,  : 
+## non-numeric argument to binary operator
+## In addition: Warning message:
+## Incompatible methods ("+.ggsurv", "+.gg") for "+" 
+
+############################################################################################
+
